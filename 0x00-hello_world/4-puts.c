6aa24c11c456e563fa611c3515612066de2d1b4f
creeds-knight <apedoarthur21@gmail.com>
2023-09-08 04:05:26 -0700

    An introduction to  c programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main. A programm that prints using puts function
+ *Return: 0 (succeful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
