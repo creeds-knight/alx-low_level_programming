9fa402fd688a6a8b43ec83a16f3de046060e7bd3
creeds-knight <apedoarthur21@gmail.com>
2023-09-10 01:55:59 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return 0;
 }
