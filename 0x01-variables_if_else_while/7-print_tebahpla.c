81f22d6eaa667e79deb59ca3021183b47ef126f8
creeds-knight <apedoarthur21@gmail.com>
2023-09-11 07:07:28 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prins the reverse alphabet
+ * Return: (0) (success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while
+		(a >= 'a') {
+			putchar(a);
+			a--;
+		}
+	putchar('\n');
+	return (0);
+}
