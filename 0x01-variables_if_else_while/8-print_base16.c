43d032bcd8215639935ef2ab3dba93e306a26072
creeds-knight <apedoarthur21@gmail.com>
2023-09-11 07:28:44 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - A program that prints hexadecimal
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	int b;
+
+	c = 'a';
+	b = 0;
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
