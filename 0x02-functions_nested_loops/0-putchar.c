6003de1c00563382c8600ec3f08f5b05b9beda1e
creeds-knight <apedoarthur21@gmail.com>
2023-09-12 10:48:50 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+ * main - A program that prints _putchar
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+
+	printword(word);
+	return (0);
+}
+
+/**
+ * printword - This function uses _putchar to print a word
+ * @word: This is the word to be printed
+ */
+void printword(char *word)
+{
+	int i;
+
+	for (i = 0; word[i] != '\0'; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+}
