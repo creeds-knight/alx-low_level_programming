32bf94c38ed8449ae447cee101644f3883d62047
creeds-knight <apedoarthur21@gmail.com>
2023-09-14 13:51:02 -0700

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints a line n the terminal
+ * @n: line to be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz for multiples of 3 and buzz for 5
+ *Return: 0(Successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	return (0);
+}
