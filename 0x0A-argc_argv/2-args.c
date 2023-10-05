8e88b2d3f252076ec87f0ad33fd1ca7c9a413853
creeds-knight <apedoarthur21@gmail.com>
2023-10-05 09:48:25 -0700

    Task 2- a program that prints the arguments passed to it on a nwe line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints all arguments it receives
+ * @argc: Number of arguments entered
+ * @argv: Array of arguments
+ * Return : 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
