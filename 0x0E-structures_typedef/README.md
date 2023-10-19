8fcea241589f776207273eb40e52be1f9e6a3847
creeds-knight <apedoarthur21@gmail.com>
2023-10-18 23:06:39 -0700

    task-0 poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This repository contains exercises on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A structure to store dog details
+ * @name: Name of the dog
+ * @age: Dog age
+ * @owner: name of the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
