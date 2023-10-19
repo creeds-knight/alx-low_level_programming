b922498c3a434330855ddc0be4635374418bf4b0
creeds-knight <apedoarthur21@gmail.com>
2023-10-19 00:41:46 -0700

    task-1,using a function to initialise the structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - A fucntion that initialises a variable of type struct dog
+ * @d: dog
+ * @name: dog name
+ * @age: age
+ * @owner: name of dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
