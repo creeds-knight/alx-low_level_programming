9c0056479d69162667c92d3ac8817d48af9323b3
creeds-knight <apedoarthur21@gmail.com>
2023-10-20 06:28:37 -0700

    task-1, printing the number of elements in a singly list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - A fucntion that returns number of elements in a list
+ * @h: A pointer to the first node
+ * Return: Number of elements in a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+	
+	while (h)
+	{
+		counter++;
+		h =h->next;
+	}
+	return (counter);
+}
