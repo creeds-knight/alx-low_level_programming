445a42fe7913265b06ca2b8278b27111d4e5b40b
creeds-knight <apedoarthur21@gmail.com>
2023-10-23 15:26:30 -0700

    task-5, first commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a lists and returns 
+ * @head: The pointer to the first element
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
