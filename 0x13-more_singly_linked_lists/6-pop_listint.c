4a0070d3e085353487ec560c05e48eb66d87834d
creeds-knight <apedoarthur21@gmail.com>
2023-10-24 02:31:26 -0700

    task-6, first commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - A function t delete the head node
+ * @head: pointer to the head
+ * Return: Head node's data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = *head;
+	if (*head != NULL)
+	{
+		free(*head);
+		(*head)->next = NULL;
+	}
+	return (temp->n);
+}
