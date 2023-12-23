4c855aae4ef6b31f6deeb20ab9a94851d41481b0
creeds-knight <apedoarthur21@gmail.com>
2023-12-23 16:20:41 +0300

    task-6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+void free_node_list(hash_node_t *head)
+{
+	hash_node_t *tmp = head;
+	hash_node_t *next;
+
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp->key);
+		free(tmp->value);
+		free(tmp);
+		tmp = next;
+	}
+}
+/**
+ * hash_table_delete - A function to delete a hash table
+ * @ht: The table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			free_node_list(ht->array[i]);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
